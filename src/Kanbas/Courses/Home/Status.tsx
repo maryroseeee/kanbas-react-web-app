bf4ad5c43c1fdb89f785b885ccf7dfdacaef4ac7
maryroseeee <mdeciutiis1@gmail.com>
2024-09-17 17:53:30 -0400

    a1

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -3,7 +3,36 @@ export default function CourseStatus() {
         <div id="wd-course-status">
             <h2>Course Status</h2>
             <button>Unpublish</button> <button>Publish</button>
-            {/* Complete on your own */}
-            <button>View Course Notifications</button>
+            <tr>
+                <td style={{ height: "5px" }}></td>
+            </tr>
+            <tr>
+                <td align="left" valign="top">
+                <button>Import Existing Content</button>
+                </td>
+            </tr>
+            <tr>
+            <td align="left" valign="top">
+                <button>Choose Home Page</button>
+            </td>
+            </tr>
+            <tr>
+            <td align="left" valign="top">
+                <button>View Course Stream</button>
+            </td>
+            </tr>
+            <td align="left" valign="top">
+                <button>New Announcement</button>
+            </td>
+        <tr>
+            <td align="left" valign="top">
+                <button>New Analytics</button>
+            </td>
+        </tr>
+        <tr>
+            <td align="left" valign="top">
+                <button>View Course Notifications</button>
+            </td>
+        </tr>
         </div>
     );}
