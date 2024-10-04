f15b4008d56c53eb395529df97c4c7e7e06b0ed5
maryroseeee <mdeciutiis1@gmail.com>
2024-10-03 23:32:17 -0400

    a2 up to 4.3

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -1,21 +1,32 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import React from "react";
+import {IoCalendarOutline} from "react-icons/io5";
+
 export default function Profile() {
     return (
         <div id="wd-profile-screen">
             <h3>Profile</h3>
-            <input id="wd-username" value="alice" placeholder="username" /><br/>
+            <input id="wd-username" value="alice" placeholder="username" className="form-control mb-2"/>
             <input id="wd-password" value="123" placeholder="password"
-                   type="password" /><br/>
-            <input id="wd-firstname" value="Alice" placeholder="First Name" /><br/>
-            <input id="wd-lastname" value="Wonderland" placeholder="Last Name" /><br/>
-            <input id="wd-dob" value="2000-01-01" type="date" /><br/>
-            <input id="wd-email" value="alice@wonderland" type="email" /><br/>
-            <select id="wd-role">
+                   type="password" className="form-control mb-2"/>
+            <input id="wd-firstname" value="Alice" placeholder="First Name" className="form-control mb-2"/>
+            <input id="wd-lastname" value="Wonderland" placeholder="Last Name" className="form-control mb-2"/>
+            <div className="input-group">
+                        <span className="input-group-text mb-2">
+                            <IoCalendarOutline className="fs-4 "/>
+                        </span>
+                <input id="wd-dob" value="2000-01-01" placeholder="mm/dd/yyyy" className="form-control mb-2"/>
+            </div>
+            <input id="wd-email" value="alice@wonderland" type="email" className="form-control mb-2"/>
+            <select id="wd-role" className="form-select mb-2">
                 <option value="USER">User</option>
                 <option value="ADMIN">Admin</option>
                 <option value="FACULTY">Faculty</option>
                 <option value="STUDENT">Student</option>
-            </select><br/>
-            <Link to="/Kanbas/Account/Signin" >Sign out</Link>
+            </select>
+
+            <button className="btn btn-danger">Sign out</button>
         </div>
-    );}
+
+    );
+}
--- src/Kanbas/Account/Signup.tsx
+++ src/Kanbas/Account/Signup.tsx
@@ -4,9 +4,19 @@ export default function Signup() {
     return (
         <div id="wd-signup-screen">
             <h3>Sign up</h3>
-            <input placeholder="username" /><br/>
-            <input placeholder="password" type="password" /><br/>
-            <input placeholder="verify password" type="password" /><br/>
-            <Link to="/Kanbas/Account/Profile" > Sign up </Link><br />
-            <Link to="/Kanbas/Account/Signin" >Sign in</Link>
-        </div> );}
+            <input id="wd-username"
+                   placeholder="username"
+                   className="form-control mb-2"/>
+            <input id="wd-password"
+                   placeholder="password" type="password"
+                   className="form-control mb-2"/>
+            <input id="wd-verify-password"
+                   placeholder="verify password" type="password"
+                   className="form-control mb-2"/>
+            <Link id="wd-signin-btn"
+                  to="/Kanbas/Account/Profile"
+                  className="btn btn-primary w-100">
+                Sign in </Link>
+            <Link id="wd-signin-link" to="/Kanbas/Account/Signin">Sign in</Link>
+        </div>
+    );}
