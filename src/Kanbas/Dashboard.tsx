a5f2fa12953e632f5ebe56b9e0806f0c804386b1
maryroseeee <91707406+maryroseeee@users.noreply.github.com>
2024-10-30 17:27:44 -0400

    a4 3.1 completed

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,10 +1,52 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as db from "./Database";
 export default function Dashboard() {
-    const courses = db.courses;
+    const [courses, setCourses] = useState<any[]>(db.courses);
+    const [course, setCourse] = useState<any>({
+        _id: "0", name: "New Course", number: "New Number",
+        startDate: "2023-09-10", endDate: "2023-12-15",
+        image: "/images/reactjs.jpg", description: "New Description"
+    });
+    const deleteCourse = (courseId: string) => {
+        setCourses(courses.filter((course) => course._id !== courseId));
+    };
+
+    const addNewCourse = () => {
+        const newCourse = { ...course,
+            _id: new Date().getTime().toString() };
+        setCourses([...courses, { ...course, ...newCourse }]);
+    };
+
+    const updateCourse = () => {
+        setCourses(
+            courses.map((c) => {
+                if (c._id === course._id) {
+                    return course;
+                } else {
+                    return c;
+                }
+            })
+        );
+    };
+
     return (
-        <div id="wd-dashboard">
+        <div className="p-4" id="wd-dashboard">
             <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+            <h5>New Course
+                <button className="btn btn-primary float-end"
+                        id="wd-add-new-course-click"
+                        onClick={addNewCourse} > Add </button>
+                <button className="btn btn-warning float-end me-2"
+                        onClick={updateCourse} id="wd-update-course-click">
+                    Update
+                </button>
+            </h5> <br />
+            <input defaultValue={course.name} className="form-control mb-2"
+                   onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+            <textarea defaultValue={course.description} className="form-control"
+                      onChange={(e) => setCourse({ ...course, description: e.target.value }) } /><hr />
+
             <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
             <div id="wd-dashboard-courses" className="row">
                 <div className="row row-cols-1 row-cols-md-5 g-4">
@@ -19,7 +61,25 @@ export default function Dashboard() {
                                             {course.name} </h5>
                                         <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
                                             {course.description} </p>
+                                        <div className="justify-content-between d-flex">
                                         <button className="btn btn-primary"> Go </button>
+                                            <button id="wd-edit-course-click"
+                                                    onClick={(event) => {
+                                                        event.preventDefault();
+                                                        setCourse(course);
+                                                    }}
+                                                    className="btn btn-warning ms-5" >
+                                                Edit
+                                            </button>
+
+                                            <button onClick={(event) => {
+                                            event.preventDefault();
+                                            deleteCourse(course._id);
+                                        }} className="btn btn-danger float-end"
+                                                id="wd-delete-course-click">
+                                            Delete
+                                        </button>
+                                    </div>
                                     </div>
                                 </Link>
                             </div>
