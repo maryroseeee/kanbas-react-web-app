2cce4fa7c19130e21d1c219aecbe67168ba6b634
maryroseeee <91707406+maryroseeee@users.noreply.github.com>
2024-11-05 19:49:19 -0500

    Merge remote-tracking branch 'origin/a4' into a4
    # Conflicts:
    #	src/Kanbas/Courses/Assignments/index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -11,6 +11,7 @@ export default function Labs() {
     return (
         <Provider store={store}>
             <div className="container-fluid">
+                <h1> Maryrose Deciutiis</h1>
                 <TOC />
                 <h1>Welcome to Web Dev!!</h1>
                 <h1>Labs</h1>
